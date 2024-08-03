eaa59c4549e38adfbad1fc7dd35bee5de33adb62
BesQpin <25635012+BesQpin@users.noreply.github.com>
2024-08-03 21:49:06 +0100

    Delete infra/manifests/shared_resource_group/.terraform directory

--- infra/manifests/shared_resource_group/.terraform/modules/rg001/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "rg001" {
-  name      = var.name
-  location  = var.location
-  tags      = merge(var.default_tags, var.custom_tags)
-}
--- infra/manifests/shared_resource_group/.terraform/modules/rg001/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "name" {
-  value = azurerm_resource_group.rg001.name
-}
-
-output "location" {
-  value = azurerm_resource_group.rg001.location
-}
