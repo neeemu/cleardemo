d2c1564a46a108d8b1b0ac642465f990f349b0ff
Peter Williams <25635012+BesQpin@users.noreply.github.com>
2024-08-03 11:40:08 +0100

    added files

---
+++ infra/manifests/app_services/app-services.tf
@@ -0,0 +1,97 @@
+
+
+
+module "as001" {
+  source = "git::https://dev.azure.com/Specsavers/DevOpsEngineering/_git/tfmodule-azurerm_app_service?ref=7.2.0"
+  #source = "C:\\Code\\cleardemo\\modules\\app_service\\"
+
+  asp_name = "${local.serial_prefix}001"
+  sku_tier = var.sku_tier
+  sku_size = var.sku_size
+  reserved = true
+  kind     = var.kind
+   
+  #tags
+  default_tags = "${local.default_tags}"
+  custom_tags  = {
+    role = "AppService - nGinx"
+  }
+
+  app_services = {
+    app1 = {
+      name                = "${local.serial_prefix}as001"
+      resource_group_name = data.azurerm_resource_group.shared_resource_group.name
+
+      # tags
+      default_tags = "${local.default_tags}"
+      role_tag     = "AppService - nGinx"
+
+      # azurerm_app_service
+      app_service_plan_id     = module.asp001.id
+      kind                    = var.kind
+      https_only              = true
+      create_app_service_plan = false
+      client_cert_enabled     = false
+
+      app_settings = {
+        PORT                                = "4000"
+        DOCKER_IMAGE                        = "DOCKER|acrem4ddave001.azurecr.io/socrates-cloud/ts-gateway:${var.typescript_socrates_gateway_artifact_version}"
+        DOCKER_REGISTRY_SERVER_URL          = "acrem4ddave001.azurecr.io"
+        DOCKER_REGISTRY_SERVER_USERNAME     = "@Microsoft.KeyVault(VaultName=${data.terraform_remote_state.key_vault.outputs.key_vault_name};SecretName=${data.azurerm_key_vault_secret.spuser001.name};SecretVersion=${data.azurerm_key_vault_secret.spuser001.version})"
+        DOCKER_REGISTRY_SERVER_PASSWORD     = "@Microsoft.KeyVault(VaultName=${data.terraform_remote_state.key_vault.outputs.key_vault_name};SecretName=${data.azurerm_key_vault_secret.sppass001.name};SecretVersion=${data.azurerm_key_vault_secret.sppass001.version})"
+        METRICS_ENABLED                     = var.metrics_enabled
+        NODE_GRAPHQL_ENDPOINTS              = local.GRAPHQL_ENDPOINTS
+        WEBSITES_CONTAINER_START_TIME_LIMIT = 600
+      }
+
+      site_config = {
+        always_on             = true
+        ftps_state            = "AllAllowed"
+        http2_enabled         = true
+        ip_restrictions       = var.network_rules_ip_rules
+        managed_pipeline_mode = "Integrated"
+        min_tls_version       = "1.2"
+        linux_fx_version      = "DOCKER|acrem4ddave001.azurecr.io/socrates-cloud/ts-gateway:${var.typescript_socrates_gateway_artifact_version}"
+        health_check_path     = "/health"
+      }
+    }
+    app2 = {
+      # common
+      name                = "${local.serial_prefix}as002"
+      resource_group_name = data.azurerm_resource_group.shared_resource_group.name
+
+      # tags
+      default_tags = var.default_tags
+      role_tag     = "AppService - nGinx"
+
+      # azurerm_app_service
+      app_service_plan_id     = module.asp001.id
+      kind                    = var.kind
+      https_only              = true
+      create_app_service_plan = false
+      client_cert_enabled     = false
+
+      app_settings = {
+        PORT                                = "4000"
+        DOCKER_IMAGE                        = "DOCKER|acrem4ddave001.azurecr.io/socrates-cloud/ts-gateway:${var.typescript_socrates_gateway_artifact_version}"
+        DOCKER_REGISTRY_SERVER_URL          = "acrem4ddave001.azurecr.io"
+        DOCKER_REGISTRY_SERVER_USERNAME     = "@Microsoft.KeyVault(VaultName=${data.terraform_remote_state.key_vault.outputs.key_vault_name};SecretName=${data.azurerm_key_vault_secret.spuser001.name};SecretVersion=${data.azurerm_key_vault_secret.spuser001.version})"
+        DOCKER_REGISTRY_SERVER_PASSWORD     = "@Microsoft.KeyVault(VaultName=${data.terraform_remote_state.key_vault.outputs.key_vault_name};SecretName=${data.azurerm_key_vault_secret.sppass001.name};SecretVersion=${data.azurerm_key_vault_secret.sppass001.version})"
+        METRICS_ENABLED                     = var.metrics_enabled
+        NODE_GRAPHQL_ENDPOINTS              = local.GRAPHQL_ENDPOINTS
+        WEBSITES_CONTAINER_START_TIME_LIMIT = 600
+      }
+
+      site_config = {
+        always_on             = true
+        ftps_state            = "AllAllowed"
+        http2_enabled         = true
+        ip_restrictions       = var.network_rules_ip_rules
+        managed_pipeline_mode = "Integrated"
+        min_tls_version       = "1.2"
+        linux_fx_version      = "DOCKER|acrem4ddave001.azurecr.io/socrates-cloud/ts-gateway:${var.typescript_socrates_gateway_artifact_version}"
+        health_check_path     = "/health"
+      }
+    }
+  }
+}
---
+++ infra/manifests/app_services/site.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "piw-platform-rg"
+    storage_account_name  = "piwtfstatestr"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate.appservice"
+    use_azuread_auth      = true
+  }
+  required_version = ">= 1.0.6"
+  required_providers {
+    azurerm = {
+      version = "~> 2.89.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
